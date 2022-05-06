f6cd0afa753e4d95a6591995505e37472e090db8
disco-praanjal <84781900+disco-praanjal@users.noreply.github.com>
2022-05-06 14:57:12 +0530

    Add files via upload

---
+++ GuessingGame.py.py
@@ -0,0 +1,14 @@
+import random 
+
+number = random
+
+while chances < 5:
+
+if guess == number :
+    print("Congratulations, you won !")
+    break
+
+if not chances < 5:
+    print("You lose ! The number was", number)
+
+
